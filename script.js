b087e0b5101add52ff160de3461c83fa71ce5dea
Abhi-nav-G <g.abhinav1205@gmail.com>
2022-01-16 10:31:21 +0530

    Will clear the page if save is clicked

--- script.js
+++ script.js
@@ -8,11 +8,26 @@ document.querySelector("#button").addEventListener("click",()=>{
     textarea.rows = 18;
     textarea.cols = 130;
     textarea.id = "text-content"
+
     let titleName = document.createElement("H2")
     titleName.innerHTML = title;
+    titleName.id = "title-name"
+
+    let saveButton = document.createElement("button")
+    saveButton.innerHTML = "SAVE";
+    saveButton.id = "save"
+    saveButton.className = "btn btn-success"
     document.getElementById("main-notes").append(titleName)
     document.getElementById("main-notes").append(textarea)
+    document.getElementById("main-notes").append(saveButton)
    //document.getElementById("main-notes").style.textAlign = center;
+
+
+   document.getElementById("save").addEventListener("click",()=>{
+    document.getElementById("title-name").remove();
+    document.getElementById("text-content").remove();
+    document.getElementById("save").remove();
+})
 })
 /*
 const createText = () =>{
